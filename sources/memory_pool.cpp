f39bd996f77121f0162eb0d2a347d6487d11a959
alpertunga-bile <kingdom19online@hotmail.com>
2025-02-03 23:19:12 +0300

    changed to forward decl

--- sources/memory_pool.cpp
+++ sources/memory_pool.cpp
@@ -1,8 +1,53 @@
 #include "../includes/memory_pool.hpp"
 
+#include <cstdint>
+#include <list>
+#include <vector>
+
 namespace pxd {
 
-MemoryPool::MemoryPool(size_t size) {
+class MemoryPool
+{
+private:
+  struct MemoryInfo
+  {
+    size_t start_index = 0;
+    size_t total_size  = 0;
+
+    MemoryInfo()                                   = default;
+    MemoryInfo(const MemoryInfo& other)            = default;
+    MemoryInfo& operator=(const MemoryInfo& other) = default;
+    MemoryInfo(MemoryInfo&& other)                 = default;
+    MemoryInfo& operator=(MemoryInfo&& other)      = default;
+    ~MemoryInfo() noexcept                         = default;
+
+    bool operator==(const MemoryInfo& other)
+    {
+      return start_index == other.start_index && total_size == other.total_size;
+    }
+
+    bool operator!=(const MemoryInfo& other)
+    {
+      return start_index != other.start_index || total_size != other.total_size;
+    }
+  };
+
+public:
+  MemoryPool() = delete;
+  explicit MemoryPool(size_t size);
+  MemoryPool(const MemoryPool& other)            = default;
+  MemoryPool& operator=(const MemoryPool& other) = default;
+  MemoryPool(MemoryPool&& other)                 = default;
+  MemoryPool& operator=(MemoryPool&& other)      = default;
+  ~MemoryPool() noexcept                         = default;
+
+private:
+  std::vector<uint8_t>  m_memory;
+  std::list<MemoryInfo> m_free_memories;
+};
+
+MemoryPool::MemoryPool(size_t size)
+{
   m_memory.resize(size);
 }
 
