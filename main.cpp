9b2669b63bc020b0ecaebfe8a37677178bcf9420
alpertunga-bile <kingdom19online@hotmail.com>
2025-02-16 00:47:47 +0300

    new test checks are added

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
+#include "tests/mem_pool_class.hpp"
 #include "includes/memory_pool.hpp"
 
 auto
 main() -> int
 {
+  /*
+  pxd::memory::MemoryPool memory(128);
+
+  auto* temp   = memory.malloc<void>(10);
+  auto* temp_2 = memory.malloc<void>(10);
+  auto* temp_3 = memory.malloc<void>(10);
+
+  memory.free(temp_3);
+  memory.free(temp);
+  memory.free(temp_2);
+
+  */
+
   pxd::memory::alloc_memory(128);
 
   void* temp   = pxd::memory::malloc(10);
   void* temp_2 = pxd::memory::malloc(10);
   void* temp_3 = pxd::memory::malloc(10);
 
-  pxd::memory::free(temp_3);
-  pxd::memory::free(temp);
   pxd::memory::free(temp_2);
+  pxd::memory::free(temp);
+  pxd::memory::free(temp_3);
+
+  auto size = pxd::memory::total_free_memory();
 
   pxd::memory::release_memory();
 
--- tests/free_tests.cpp
+++ tests/free_tests.cpp
@@ -58,6 +58,31 @@ TEST(Free, NextMemory)
   pxd::memory::release_memory();
 }
 
+TEST(Free, NextMemory2)
+{
+  pxd::memory::alloc_memory(128);
+
+  void* temp   = pxd::memory::malloc(10);
+  void* temp_2 = pxd::memory::malloc(10);
+  void* temp_3 = pxd::memory::malloc(10);
+  void* temp_4 = pxd::memory::malloc(10);
+  void* temp_5 = pxd::memory::malloc(10);
+
+  EXPECT_EQ(78, pxd::memory::total_free_memory());
+
+  pxd::memory::free(temp_2);
+
+  EXPECT_EQ(88, pxd::memory::total_free_memory());
+  EXPECT_EQ(10, pxd::memory::min_free_memory());
+
+  pxd::memory::free(temp_3);
+
+  EXPECT_EQ(98, pxd::memory::total_free_memory());
+  EXPECT_EQ(20, pxd::memory::min_free_memory());
+
+  pxd::memory::release_memory();
+}
+
 TEST(Free, MiddleMemory)
 {
   pxd::memory::alloc_memory(128);
@@ -81,6 +106,36 @@ TEST(Free, MiddleMemory)
   pxd::memory::free(temp_2);
 
   EXPECT_EQ(128, pxd::memory::total_free_memory());
+
+  pxd::memory::release_memory();
+}
+
+TEST(Free, MiddleMemory2)
+{
+  pxd::memory::alloc_memory(128);
+
+  void* temp   = pxd::memory::malloc(10);
+  void* temp_2 = pxd::memory::malloc(10);
+  void* temp_3 = pxd::memory::malloc(10);
+  void* temp_4 = pxd::memory::malloc(10);
+  void* temp_5 = pxd::memory::malloc(10);
+
+  EXPECT_EQ(78, pxd::memory::total_free_memory());
+
+  pxd::memory::free(temp_2);
+
+  EXPECT_EQ(88, pxd::memory::total_free_memory());
+  EXPECT_EQ(10, pxd::memory::min_free_memory());
+
+  pxd::memory::free(temp_4);
+
+  EXPECT_EQ(98, pxd::memory::total_free_memory());
+  EXPECT_EQ(10, pxd::memory::min_free_memory());
+
+  pxd::memory::free(temp_3);
+
+  EXPECT_EQ(108, pxd::memory::total_free_memory());
+  EXPECT_EQ(30, pxd::memory::min_free_memory());
 
   pxd::memory::release_memory();
 }
